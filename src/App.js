a057672e5706b0a800454642de99644060931c35
Saravanan <ksaravanakumarkumaresan@gmail.com>
2022-12-26 23:12:45 +0530

    Redux without middleware

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Header } from './Containers/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ProductListing } from './Containers/ProuctListing';
+import { ProductDetails } from './Containers/ProductDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <Router>
+        <Header />
+      <Routes>
+        <Route path="/" element={<ProductListing />} />
+        <Route path="/products/:productsId" element={<ProductDetails />} />
+        <Route> 404 Not Found</Route>
+      </Routes>
+    </Router>
+     
+  </div>
   );
 }
 
